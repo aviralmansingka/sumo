34e5b9ebc1271dbe6cda5d217f162bb25e4650c4
Aviral Mansingka <aviralmansingka@gmail.com>
2016-05-21 15:44:24 -0700

    Stuff

--- routes/webhook.js
+++ routes/webhook.js
@@ -16,18 +16,11 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
 	if(req.body) {
 		console.log('There is some stuff in here ' + req.body);
+		res.send(req.body);
 	}else{
-		console.log('Something went wrong')
+		console.log('Something went wrong');
+		res.send(req.body);
 	}
-	request.post(
-	    'http://www.yoursite.com/formpage',
-	    { form: { key: 'value' } },
-	    function (error, response, body) {
-	        if (!error && response.statusCode == 200) {
-	            console.log(body)
-	        }
-	    }
-	);
 })
 
 module.exports = router;
