ee0f4eb419c499dabe1637dfc732b4afddd65532
Aviral Mansingka <aviralmansingka@gmail.com>
2016-05-21 15:55:39 -0700

    trying to log out sent text

--- routes/webhook.js
+++ routes/webhook.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
 	console.log('YOYOYO');
+	console.log(req.body.entry[0].messaging[0].message.text);
 	res.end();
 })
 
