12addae21bae02c6ca79b63cea1a00828498dc20
Aviral Mansingka <aviralmansingka@gmail.com>
2016-05-22 02:17:06 -0700

    Added wit engine

---
+++ routes/wit_engine.js
@@ -0,0 +1,18 @@
+module.exports = function(text) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        method: 'GET',
+        json: {
+            q : text,
+            access_token: 'S3X5IJC2Q5VC45Q6XFK326COB5673RLN'
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }else {
+        	console.log(body)
+        }
+    })
+}
