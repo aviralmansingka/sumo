e0cee7b27f40a41502328ddd2ea1388c47ee40a1
Aviral Mansingka <aviralmansingka@gmail.com>
2016-05-22 02:38:24 -0700

    changed structure of get request

--- routes/wit_engine.js
+++ routes/wit_engine.js
@@ -2,11 +2,8 @@ var request = require('request');
 module.exports = function(text) {
     request({
         url: 'https://api.wit.ai/message',
-        method: 'GET',
-        json: {
-            q : text,
-            access_token: 'BRWV5Z7ZQSEQZ4CTDSJ2HSFN5X6UCYSA'
-        }
+        qs: {access_token:'BRWV5Z7ZQSEQZ4CTDSJ2HSFN5X6UCYSA'},
+        method: 'GET'
     }, function(error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
