fc2462eaf38d99d8c2727c87cf49b7f91a4f1ed8
Aviral Mansingka <aviralmansingka@gmail.com>
2016-05-21 19:50:28 -0700

    Removing all code

--- routes/webhook.js
+++ routes/webhook.js
@@ -18,10 +18,10 @@ router.post('/', function(req, res) {
 	var text = (req.body.entry[0].messaging[0].message.text);
 	console.log(text);
 	console.log(sender_id);
-	var page_token = 'EAAQoyn1s0fMBAGjI17aXtHKPD4QGFfDk0wNstXCFNfHJkaDIQvdQHVJcZBZCfBOhcJjttZBYQVPeDyFZAHr8gDj3ria7iPncJb8wAMARhl0JWiynTXmtKRv5jw8qw54DXCmMuITUMPdmlhlyP8pqgLNLlaTBNubNzGzFn48ZAtAZDZD';
+	//var page_token = 'EAAQoyn1s0fMBAGjI17aXtHKPD4QGFfDk0wNstXCFNfHJkaDIQvdQHVJcZBZCfBOhcJjttZBYQVPeDyFZAHr8gDj3ria7iPncJb8wAMARhl0JWiynTXmtKRv5jw8qw54DXCmMuITUMPdmlhlyP8pqgLNLlaTBNubNzGzFn48ZAtAZDZD';
 
-	var url = "https://graph.facebook.com/v2.6/me/messages?access_token="+page_token;
-	console.log(page_token);
+	//var url = "https://graph.facebook.com/v2.6/me/messages?access_token="+page_token;
+	//console.log(page_token);
 	
 	
 })
