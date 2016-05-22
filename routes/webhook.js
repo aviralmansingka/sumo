1948bebdd89eca029d9149c6b6517e018b19443e
Aviral Mansingka <aviralmansingka@gmail.com>
2016-05-21 19:54:01 -0700

    why isn't this working

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-var page_token = 'EAAQoyn1s0fMBAGjI17aXtHKPD4QGFfDk0wNstXCFNfHJkaDIQvdQHVJcZBZCfBOhcJjttZBYQVPeDyFZAHr8gDj3ria7iPncJb8wAMARhl0JWiynTXmtKRv5jw8qw54DXCmMuITUMPdmlhlyP8pqgLNLlaTBNubNzGzFn48ZAtAZDZD';
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -18,12 +18,7 @@ router.post('/', function(req, res) {
 	var text = (req.body.entry[0].messaging[0].message.text);
 	console.log(text);
 	console.log(sender_id);
-	//var page_token = 'EAAQoyn1s0fMBAGjI17aXtHKPD4QGFfDk0wNstXCFNfHJkaDIQvdQHVJcZBZCfBOhcJjttZBYQVPeDyFZAHr8gDj3ria7iPncJb8wAMARhl0JWiynTXmtKRv5jw8qw54DXCmMuITUMPdmlhlyP8pqgLNLlaTBNubNzGzFn48ZAtAZDZD';
-
-	//var url = "https://graph.facebook.com/v2.6/me/messages?access_token="+page_token;
-	//console.log(page_token);
 	res.end();
-	
 })
 
 module.exports = router;
